ceb58f90782023ed4b4fe3c2fb362b49deffa07d
DaliaDandashi <78696190+DaliaDandashi@users.noreply.github.com>
2021-03-09 22:47:48 +0200

    step_8Done

--- javascript-basics-01/Step_8.js
+++ javascript-basics-01/Step_8.js
@@ -2,6 +2,8 @@ var age;
 age=prompt("Enter your age : ");
 if (age>=18){
     window.alert("you are over 18 ");
-    }else{
+    }
+    else
+    {
         window.alert("you are under 18 ");
     };
